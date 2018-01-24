1e58063ab968699b22626f8f51b1e82c28bf180e
Lauren Burroughs <burroughs@palantir.net>
2018-01-24 12:49:37 -0600

    mobile hide/show share interaction

--- styleguide/source/assets/js/sticky-share.js
+++ styleguide/source/assets/js/sticky-share.js
@@ -11,7 +11,7 @@
    Drupal.behaviors.sticky_share = {
      attach: function (context, settings) {
 
-         // Opens and closes tool drawer. 
+         // Opens and closes tool drawer.
          // Closes other drawers when selected one opens.
          $('#joe__share--trigger').click(function() {
            // Unfocus on the dropdown
@@ -19,7 +19,39 @@
            // add a class to the sibling dropdown
            $(this).siblings('.joe__share').slideToggle(100);
          });
-             
+
+         // sticky sticky-share
+         $(window).scroll(function(){
+            var winTop = $(window).scrollTop();
+            if(winTop >= 30){
+              $("body").addClass("joe__sticky-share");
+            }else{
+              $("body").removeClass("joe__sticky-share");
+            }//if-else
+          });//win func.
+
+          $.fn.isInViewport = function() {
+            var elementTop = $(this).offset().top;
+            var elementBottom = elementTop + $(this).outerHeight();
+
+            var viewportTop = $(window).scrollTop();
+            var viewportBottom = viewportTop + $(window).height();
+
+            return elementBottom > viewportTop && elementTop < viewportBottom;
+          };
+
+          $(window).on('resize scroll', function() {
+            $('.joe__issue-teaser').each(function() {
+              if ($(this).isInViewport()) {
+                $('body').removeClass('joe__sticky-share');
+              }
+            });
+            $('.joe__footer').each(function() {
+              if ($(this).isInViewport()) {
+                $('body').removeClass('joe__sticky-share');
+              }
+            });
+          });
      }
    };
  })(jQuery, Drupal);
