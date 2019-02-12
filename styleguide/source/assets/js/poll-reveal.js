bf483f8b86c73ebec5aeee84188522da77f4c851
Adrian Muresan <muresan@palantir.net>
2019-02-12 14:24:51 -0600

    EWLJ-316: modify js in order to try to make it work in d8

--- styleguide/source/assets/js/poll-reveal.js
+++ styleguide/source/assets/js/poll-reveal.js
@@ -7,20 +7,44 @@
  * - https://drupal.org/node/1446420
  * - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
  */
- (function ($, Drupal) {
-   Drupal.behaviors.pollReveal = {
-     attach: function (context, settings) {
-        // Opens and closes results. 
-        $('.joe__poll__reveal').click(function(e) {
-          e.preventDefault();
-          e.stopPropagation();
-          // Unfocus on the dropdown
-          $(this).blur();
-          // add a class to the sibling dropdown
-          $(this).parents('.joe__poll__answers').toggleClass('is-open');
-          $(this).toggleClass('is-active'); 
-        });
+(function ($, Drupal) {
+  Drupal.behaviors.pollReveal = {
+    attach: function (context, settings) {
+      // Opens and closes results. 
+      $(document).ready(function () {
+        $('.joe__poll__reveal').unbind('click').bind('click', (function (event) {
+        console.log('test');
+        // Prevents the default event behavior (ie: click).
+        event.preventDefault();
+        // Prevents the event from propagating (ie: "bubbling").
+        event.stopPropagation();
+        // Unfocus on the dropdown
+        $(this).blur();
+        // add a class to the sibling dropdown  
+        $(this).parents('.joe__poll__answers').toggleClass('is-open');
+        $(this).toggleClass('is-active');
+      }));
+    });
+   }
+  }
+})(jQuery, Drupal);
 
-     }
-   };
- })(jQuery, Drupal);
+// (function ($, Drupal) {
+//   Drupal.behaviors.pollReveal = {
+//     attach: function (context, settings) {
+//  
+//       $('.joe__poll__reveal', context).once('pollReveal').unbind('click').bind('click', (function (event) {
+//           console.log('test');
+//           // Prevents the default event behavior (ie: click).
+//           event.preventDefault();
+//           // Prevents the event from propagating (ie: "bubbling").
+//           event.stopPropagation();
+//           // Unfocus on the dropdown
+//           $(this).blur();
+//           // add a class to the sibling dropdown  
+//           $(this).parents('.joe__poll__answers').toggleClass('is-open');
+//           $(this).toggleClass('is-active');
+//         }));
+//     }
+//   }
+// })(jQuery, Drupal);
