236cf4e5f105ce478260da335f75d5600775d66f
Manning Peterson <manning.peterson@ama-assn.org>
2019-03-13 15:07:48 -0500

    Update 2019-03-13T20:07:38.319Z

--- assets/js/poll-reveal.js
+++ assets/js/poll-reveal.js
@@ -7,20 +7,29 @@
  * - https://drupal.org/node/1446420
  * - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
  */
- (function ($, Drupal) {
-   Drupal.behaviors.pollReveal = {
-     attach: function (context, settings) {
-        // Opens and closes results. 
-        $('.joe__poll__reveal').click(function(e) {
-          e.preventDefault();
-          e.stopPropagation();
+(function ($, Drupal) {
+  Drupal.behaviors.pollReveal = {
+    attach: function (context, settings) {
+      // Opens and closes results. 
+      $(document).ready(function () {
+        $('.joe__poll__reveal').unbind('click').bind('click', (function (event) {
+          // Prevents the default event behavior (ie: click).
+          event.preventDefault();
+          // Prevents the event from propagating (ie: "bubbling").
+          event.stopPropagation();
           // Unfocus on the dropdown
-          $(this).blur();
-          // add a class to the sibling dropdown
-          $(this).parents('.joe__poll__answers').toggleClass('is-open');
-          $(this).toggleClass('is-active'); 
-        });
-
-     }
-   };
- })(jQuery, Drupal);
+        $(this).blur();
+        // add a class to the sibling dropdown  
+        $(this).parents('.joe__poll__answers').toggleClass('is-open');
+        $(this).toggleClass('is-active');
+      }));
+      // Updates show/hide text.
+      $('.joe__poll__answers').each(function(i, obj) {
+          if($(this).is(".is-open")){
+	          $(this).find('.joe__poll__reveal').addClass('is-active');
+          }
+      });
+    });
+   }
+  }
+})(jQuery, Drupal);
