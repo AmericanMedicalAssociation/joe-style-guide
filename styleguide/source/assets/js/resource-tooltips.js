cb8427dfa5a0b5889895b16d6f50f6c19e0a7465
Neil Gallagher <neil.gallagher@ama-assn.org>
2019-05-06 16:52:17 -0500

    Adds method to check for multiple sup tags and separate them out

--- styleguide/source/assets/js/resource-tooltips.js
+++ styleguide/source/assets/js/resource-tooltips.js
@@ -9,27 +9,41 @@
  */
  (function ($, Drupal) {
    Drupal.behaviors.resourceToolips = {
-     attach: function (context, settings) {
-       $('article sup').hover(
-         function() {
-           // Find the <sup> tag number and convert it into an integer
-           var $supNumber = parseInt($(this).text()) - 1;
-           // Take <sup> number and use it to get the reference
-           var $reference = $('.joe__references__list li').eq($supNumber);
-           // Prevent the hover function from cloning the reference more than once
-           if(!$(this).find('.ama__tooltip').length){
-             // Append a div with the reference to the <sup>
-             $(this).append('<div class="ama__tooltip">' + $reference.html() + '</div>');
-             // Show the reference tooltip
-             $(this).children('.ama__tooltip').fadeIn()
-           } else {
-             $(this).find('.ama__tooltip').fadeIn();
-           }
-         }, function() {
-           // Hide reference tooltip
-           $(this).find('.ama__tooltip').fadeOut();
+     attach: function () {
+       // Separates the sup tags that have multiple numbers into individual sup tags
+       $('p sup').each(function(){
+         var $sup = $(this);
+         var supText = $sup.text().trim();
+         if (supText.indexOf(',') > -1) {
+           $sup.replaceWith($sup.html().split(',').map(function(el, i) {
+             // If the sup number is the last one it should not have a comma after it
+             return $sup.length - 1 === i ? '<sup>' + el + ', </sup>' : '<sup>' + el + '</sup>'
+           }));
          }
-       );
+       });
+      // Sup on hover connect it to the references
+       $('article sup').hover(
+        function() {
+          // Find the <sup> tag number and convert it into an integer
+          var $supNumber = parseInt($(this).text()) - 1;
+          // Take <sup> number and use it to get the reference
+          var $reference = $('.joe__references__list li').eq($supNumber);
+          // Prevent the hover function from cloning the reference more than once
+          if(!$(this).find('.ama__tooltip').length){
+            // Append a div with the reference to the <sup>
+            $(this).append('<div class="ama__tooltip">' + $reference.html() + '</div>');
+            // Show the reference tooltip
+            $(this).children('.ama__tooltip').fadeIn()
+          } else {
+            $(this).find('.ama__tooltip').fadeIn();
+          }
+        }, function() {
+          // Hide reference tooltip
+          $(this).find('.ama__tooltip').fadeOut();
+        }
+      );
      }
    };
  })(jQuery, Drupal);
+
+
