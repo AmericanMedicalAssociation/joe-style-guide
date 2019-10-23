a524a4b3f6bd794fe2fa54d967d1f386774f82dc
nstriedinger <striedinger@palantir.net>
2019-10-23 10:13:07 -0400

    restructure image expander

--- styleguide/source/assets/js/lightbox.js
+++ styleguide/source/assets/js/lightbox.js
@@ -8,38 +8,32 @@
  * - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
  */
 (function ($, Drupal) {
+  'use strict';
+
   Drupal.behaviors.lightbox = {
     attach: function (context, settings) {
-        
-      var $savedScrollPosition = 0;
       // Opens and closes lightbox.
-      $('.joe__inline-image__zoom').click(function() {
-        event.preventDefault();
+      $('.joe__inline-image__zoom').click(function(e) {
+        e.preventDefault();
+        e.stopPropagation();
         // Unfocus on the dropdown
         $(this).blur();
         // add a class to the sibling dropdown
         $(this).parents('.joe__inline-image').toggleClass('is-zoomed');
-        // While image is zoomed-in, save the scroll position and freeze scrolling.
-        if ($('.layout-container').hasClass('freeze-scrolling')) {
-          $('.layout-container').removeClass('freeze-scrolling');
-          // Restore scroll position before image was magnified.
-          $(document).scrollTop($savedScrollPosition);
-          $savedScrollPosition = 0;
-        } else {
-          // Save scroll position during 'freeze-scrolling'.
-          $savedScrollPosition = $(document).scrollTop();
-          $('.layout-container').addClass('freeze-scrolling');
-        }
       });
       
-      // close the modal when esc is pressed
-      $(document).keydown(function(event) { 
-        if (event.keyCode == 27) { 
-          $('.layout-container').removeClass('freeze-scrolling');
-          if ($savedScrollPosition) {
-            // Restore scroll position before image was magnified.
-            $(document).scrollTop($savedScrollPosition);
-          }
+      // Close the modal when esc is pressed.
+      $(document).on('keydown', function(e) {
+        if (e.keyCode === 27) {
+          $(".joe__inline-image").removeClass('is-zoomed');
+        }
+      });
+
+      // Close when clicking outside of zoomed image.
+      $(document).on('click', function(e) {
+        e.preventDefault();
+        var target = $(e.target);
+        if (target.is(':not(img)')) {
           $(".joe__inline-image").removeClass('is-zoomed');
         }
       });
