efacd331ce8a3e3c176fc1b05545c0f2a971fb20
dr-zee <e-blast@joshthorne.com>
2019-12-11 11:36:41 -0600

    Update 2019-12-11T17:36:21.360Z

--- assets/js/lightbox.js
+++ assets/js/lightbox.js
@@ -8,35 +8,45 @@
  * - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
  */
 (function ($, Drupal) {
-  'use strict';
+  'use strict'
 
   Drupal.behaviors.lightbox = {
     attach: function (context, settings) {
       // Opens and closes lightbox.
-      $('.joe__inline-image__zoom').click(function(e) {
-        e.preventDefault();
-        e.stopPropagation();
+      $('.joe__inline-image__zoom').click(function (e) {
+        e.preventDefault()
+        e.stopPropagation()
         // Unfocus on the dropdown
-        $(this).blur();
+        $(this).blur()
+        var $width = $('.layout-container').width()
         // add a class to the sibling dropdown
-        var $figure = $(this).closest('.joe__inline-image');
-        $figure.toggleClass('is-zoomed');
-        $figure.css('max-width', $('.layout-container').width());
-      });
+        var $figure = $(this).closest('.joe__inline-image')
+        $figure.toggleClass('is-zoomed')
+
+        if ($figure.hasClass('is-zoomed')) {
+          $figure.css('width', $width)
+          $('.contextual-region').css('position', 'static')
+        } else {
+          $figure.removeAttr('style')
+          $('.contextual-region').css('position', 'relative')
+        }
+      })
 
       // Close the modal when esc is pressed.
-      $(document).on('keydown', function(e) {
+      $(document).on('keydown', function (e) {
         if (e.keyCode === 27) {
-          $(".joe__inline-image").removeClass('is-zoomed');
+          $('.joe__inline-image').removeClass('is-zoomed').removeAttr('style')
+          $('.contextual-region').css('position', 'relative')
         }
-      });
+      })
 
-      $(document).on('click', function(e) {
-         var target = $(e.target);
-         if (target.is(':not(img)')) {
-           $(".joe__inline-image").removeClass('is-zoomed');
-         }
-      });
+      $(document).on('click', function (e) {
+        var target = $(e.target)
+        if (target.is(':not(img)')) {
+          $('.joe__inline-image').removeClass('is-zoomed').removeAttr('style')
+          $('.contextual-region').css('position', 'relative')
+        }
+      })
     }
-  };
-})(jQuery, Drupal);
+  }
+})(jQuery, Drupal)
