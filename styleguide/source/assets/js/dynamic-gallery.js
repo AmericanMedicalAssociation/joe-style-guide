5e79e48bf1a4883a77f96ab7d32799eb9bee6d5d
dr-zee <e-blast@joshthorne.com>
2020-09-22 21:28:05 -0500

    Added scss partial, waypoint and inview js.

---
+++ styleguide/source/assets/js/dynamic-gallery.js
@@ -0,0 +1,14 @@
+/**
+ * @file
+ *
+ *
+ *
+ */
+
+(function ($, Drupal) {
+  Drupal.behaviors.gammery = {
+    attach: function (context, settings) {
+      console.log('sup short bros');
+    }
+  };
+})(jQuery, Drupal);
