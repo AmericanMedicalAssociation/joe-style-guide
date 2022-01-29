9ae88b2a1ac4ff965ab58085644565b4cdff198c
dr-zee <e-blast@joshthorne.com>
2022-01-28 21:31:04 -0600

    gallery js and styles.

--- styleguide/source/assets/js/vendor/gallery.js
+++ styleguide/source/assets/js/vendor/gallery.js
@@ -2,53 +2,53 @@
   Drupal.behaviors.gallery = {
     attach: function (context, settings) {
 
-      // Down button
+      $.fn.setViewportHeight = function() {
+        var viewportHeight = $('.image-wrapper').height();
+        $('.image-wrapper svg').css('height', viewportHeight);
+      };
       $('.scroll-down').on('click', function(e) {
         e.preventDefault();
         fullpage_api.moveSectionDown();
       });
 
       var svg = document.querySelector('.image-wrapper svg');
+      var anchorList = [];
+      var coordList = [];
+
+      // Initialize the first section to trigger correct first scroll animation
+      $( document ).ready(function() {
+        $.fn.setViewportHeight();
+        // Add each data-anchor to an array for fullpage.js
+        $('div[data-anchor]').each(function(idx, el){
+          anchorList.push($(this).attr('data-anchor'));
+        });
+        // Add each coordinate to an array
+        $('div[data-coordinates]').each(function(idx, el){
+          coordList.push($(this).attr('data-coordinates'));
+        });
+      });
+
+      $(window).resize(function() {
+        $.fn.setViewportHeight();
+      });
+
       var gallery = gsap.timeline({
         repeat: false,
         defaults: {
           duration: 1.3,
           ease: 'power2.out'
         }
-
-        // Get visual viewbox coordinates for development
-        //onUpdate: () => {
-        //const x = Math.floor(svg.viewBox.baseVal.x);
-        //const y = Math.floor(svg.viewBox.baseVal.y);
-        //const width = Math.floor(svg.viewBox.baseVal.width);
-        //const height = Math.floor(svg.viewBox.baseVal.height);
-        //p.innerHTML = 'viewBox="${x} ${y} ${width} ${height}"';
-        //}
       });
 
-      // Initialize the first section to trigger correct first scroll animation
-      $( document ).ready(function() {
-        var viewBox = $('.image-wrapper svg').attr('viewBox');
-        console.log(viewBox);
-        gallery.to(svg, {
-          attr: {
-            viewBox: viewBox
-          }
-        });
+      var viewBox = $('.image-wrapper svg').attr('viewBox');
+      gallery.to(svg, {
+        attr: {
+          viewBox: viewBox
+        }
       });
-
       function initialization() {
         var myFullpage = new fullpage('#fullpage', {
-          anchors: [
-            'part-1',
-            'part-2',
-            'part-3',
-            'part-4',
-            'part-5',
-            'part-6',
-            'part-7',
-            'footer'
-          ],
+          anchors: anchorList,
           licenseKey: '77D14527-65F84765-8BF82EDE-2814ECA6',
           scrollOverflow: true,
           navigation: false,
@@ -61,6 +61,11 @@
               destination:destination,
               direction: direction
             };
+            gallery.to(svg, {
+              attr: {
+                viewBox: coordList[destination.index]
+              }
+            });
 
             //  When you leave the first slide
             if (destination.index !== 0) {
@@ -68,69 +73,13 @@
               $('.top').addClass('visible');
             } else {
               $('.top').removeClass('visible');
-            }
-
-            if (destination.index === 0) {
-              gallery.to(svg, {
-                attr: {
-                  viewBox: '352 385 954 954'
-                }
-              });
               $('.gallery-header').removeClass('joe__sticky-gallery-header');
-            }
-            if (destination.index === 1) {
-              gallery.to(svg, {
-                attr: {
-                  viewBox: '623 755 843 843'
-                }
-              });
-            }
-            if (destination.index === 2) {
-              gallery.to(svg, {
-                attr: {
-                  viewBox: '65 755 843 843'
-                }
-              });
-            }
-            if (destination.index === 3) {
-              gallery.to(svg, {
-                attr: {
-                  viewBox: '246 387 1188 1188'
-                }
-              });
-            }
-            if (destination.index === 4) {
-              gallery.to(svg, {
-                attr: {
-                  viewBox: '623 755 843 843'
-                }
-              });
-            }
-            if (destination.index === 5) {
-              gallery.to(svg, {
-                attr: {
-                  viewBox: '65 755 843 843'
-                }
-              });
-            }
-            if (destination.index === 6) {
-              gallery.to(svg, {
-                attr: {
-                  viewBox: '246 387 1188 1188'
-                }
-              });
-            }
-            if (destination.index === 7) {
-              gallery.to(svg, {
-                attr: {
-                  viewBox: '65 755 843 843'
-                }
-              });
             }
           }
         });
       }
       initialization();
+      $('.text-column').removeClass('loading').addClass('loaded');
     }
   };
 })(jQuery, Drupal);
