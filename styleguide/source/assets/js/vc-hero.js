0d632633115feb6f181fd8b35cf21f78dea82cd9
Carl Martens <carlrmartens@gmail.com>
2023-10-16 20:41:05 -0500

    Added citation toggle. Image variables

---
+++ styleguide/source/assets/js/vc-hero.js
@@ -0,0 +1,29 @@
+/**
+ * @file
+ * Facets and filters
+ *
+ * JavaScript should be made compatible with libraries other than jQuery by
+ * wrapping it with an "anonymous closure". See:
+ * - https://drupal.org/node/1446420
+ * - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
+ */
+
+(function ($, Drupal) {
+  Drupal.behaviors.vc_hero_art = {
+    attach: function (context, settings) {
+
+      // Global header variables
+      const vcToolBtn = document.querySelector('.vc-hero__tool-button');
+      const vcToolContent = document.querySelector('.vc-hero__tool-content');
+
+      if (vcToolContent) {
+        vcToolBtn.addEventListener('click', function () {
+          const expanded = this.getAttribute("aria-expanded") === "true" || false;
+          this.setAttribute("aria-expanded", !expanded);
+          const content = this.nextElementSibling;
+          content.hidden = !content.hidden;
+        });
+      }
+    }
+  };
+})(jQuery, Drupal);
