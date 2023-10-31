b3f451526d0b2c826bdfabdfaf0f48a9ee1fd675
Carl Martens <carlrmartens@gmail.com>
2023-10-31 14:49:08 -0500

    Updated js to handle multiple instances of reactions on a page

--- styleguide/source/assets/js/vc-reaction-bar.js
+++ styleguide/source/assets/js/vc-reaction-bar.js
@@ -13,36 +13,41 @@
     attach: function (context, settings) {
 
       // Global variables
-      const vcReactionBtn = document.querySelector('.vc-reaction-bar__emojis-button');
-      const vcReactionEmojis = document.querySelector('.vc-reaction-bar__emojis-dropdown');
+      const vcReactionBtn = document.querySelectorAll('.vc-reaction-bar__emojis-button');
+      const vcReactionEmojis = document.querySelectorAll('.vc-reaction-bar__emojis-dropdown');
 
       if (vcReactionEmojis) {
-        vcReactionBtn.addEventListener('click', function () {
-          const expanded = this.getAttribute('aria-expanded') === 'true' || false;
-          this.setAttribute('aria-expanded', !expanded);
-          vcReactionEmojis.hidden = !vcReactionEmojis.hidden;
+        vcReactionBtn.forEach(e => {
+          e.addEventListener('click', function () {
+            const expanded = e.getAttribute('aria-expanded') === 'true' || false;
+            e.setAttribute('aria-expanded', !expanded);
+            e.nextElementSibling.hidden = !e.nextElementSibling.hidden;
+          });
+        });
 
-          // On `esc` press hide open `vcReactionEmojis`
-          document.addEventListener('keyup', (e) => {
-            if (!vcReactionEmojis.hidden) {
+        // On `esc` press hide open `vcReactionEmojis`
+        document.addEventListener('keyup', (e) => {
+          vcReactionEmojis.forEach(emoji => {
+            if (!emoji.hidden) {
               const key = e.key || e.keyCode;
               if (key === 'Escape' || key === 'Esc' || key === 27) {
-                vcReactionBtn.setAttribute('aria-expanded', 'false');
-                vcReactionEmojis.hidden = true;
+                emoji.hidden = true;
+                emoji.previousElementSibling.setAttribute('aria-expanded', 'false');
               }
             }
           });
-
-          // Close `vcReactionEmojis` when mouseup outside of elements
-          document.addEventListener('mouseup', function (e) {
-            if (!e.target.closest('.vc-reaction-bar__emojis-button') && !e.target.closest('.vc-reaction-bar__emojis-dropdown')) {
-              vcReactionBtn.setAttribute('aria-expanded', 'false');
-              vcReactionEmojis.hidden = true;
-            }
-          }, false);
         });
-      }
 
+        // Close `vcReactionEmojis` when mouseup outside of elements
+        document.addEventListener('mouseup', function (e) {
+          if (!e.target.closest('.vc-reaction-bar__emojis-button') && !e.target.closest('.vc-reaction-bar__emojis-dropdown')) {
+            vcReactionBtn.forEach(e => {
+              e.setAttribute('aria-expanded', 'false');
+              e.nextElementSibling.hidden = true;
+            });
+          }
+        }, false);
+      }
     }
   };
 })(jQuery, Drupal);
