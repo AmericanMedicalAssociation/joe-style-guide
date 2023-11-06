e42ca4aa0033aa7ac48b0533520f6df87108347e
Carl Martens <carlrmartens@gmail.com>
2023-11-06 10:05:10 -0600

    Added funtion to window load

--- styleguide/source/assets/js/vc-page.js
+++ styleguide/source/assets/js/vc-page.js
@@ -11,17 +11,22 @@
 (function ($, Drupal) {
   Drupal.behaviors.vc_page = {
     attach: function (context, settings) {
-
       // Find all inViewSection classes
-      const inViewSection = document.querySelectorAll(':root[class*=vc-] article .c-paragraph');
+      const inViewSection = document.querySelectorAll(
+        ":root[class*=vc-] article .c-paragraph"
+      );
 
       // Find the dimensions of inPageView
       const inPageView = function (e) {
         const distance = e.getBoundingClientRect();
         return (
-          distance.top <= (window.innerHeight - 200 || document.documentElement.clientHeight - 200) &&
+          distance.top <=
+            (window.innerHeight - 200 ||
+              document.documentElement.clientHeight - 200) &&
           distance.left >= 0 &&
-          distance.right <= (window.innerWidth || document.documentElement.clientWidth))
+          distance.right <=
+            (window.innerWidth || document.documentElement.clientWidth)
+        );
       };
 
       // Set inView
@@ -29,7 +34,7 @@
         for (let i = 0; i < inViewSection.length; i++) {
           if (inPageView(inViewSection[i])) {
             // If in view
-            inViewSection[i].classList.add('in-view');
+            inViewSection[i].classList.add("in-view");
           }
         }
       }
@@ -40,20 +45,28 @@
         let lastScrollPosition = 0;
         let throttle = false;
 
-        window.addEventListener('scroll', () => {
-          // Set lastScrollPosition to window.scrollY
-          lastScrollPosition = window.scrollY;
-          // Throttle scroll behavior
-          if (!throttle) {
-            setTimeout(() => {
-              sectionInView(lastScrollPosition);
-              throttle = false;
-            }, 25);
-          }
-          throttle = true;
-        }, false);
-      }
+        window.addEventListener(
+          "scroll",
+          () => {
+            // Set lastScrollPosition to window.scrollY
+            lastScrollPosition = window.scrollY;
+            // Throttle scroll behavior
+            if (!throttle) {
+              setTimeout(() => {
+                sectionInView(lastScrollPosition);
+                throttle = false;
+              }, 25);
+            }
+            throttle = true;
+          },
+          false
+        );
 
-    }
+        window.addEventListener("load", () => {
+          sectionInView();
+          console.log("loaded");
+        });
+      }
+    },
   };
 })(jQuery, Drupal);
